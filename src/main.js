88adf6fa9857cb98f651042360d42689d62c1bc2
NERON147 <faip98.07.5@gmail.com>
2022-09-11 14:33:26 +0300

    qq

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import router from './routes'
+import store from './store'
+
 
 import './assets/scss/main.scss'
 
@@ -9,5 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app')
